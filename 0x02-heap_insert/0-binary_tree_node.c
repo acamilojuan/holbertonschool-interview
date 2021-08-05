20bc734ea4b79b8b65694afcdaa47e65b25a12f0
Juan Camilo Acevedo <60371540+acamilojuan@users.noreply.github.com>
2021-08-05 08:20:20 -0500

    Fixing syntax

--- 0x02-heap_insert/0-binary_tree_node.c
+++ 0x02-heap_insert/0-binary_tree_node.c
@@ -1,6 +1,6 @@
 #include <stdlib.h>
 #include "binary_trees.h"
-/*
+/**
  * binary_tree_node - Fx to create a binary tree node
  * @value: value to put in the new node
  * @parent: pointer to the parent node of the node to create
@@ -11,15 +11,12 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 	binary_tree_t *newn;
 
 	newn = malloc(sizeof(binary_tree_t));
-	
+
 	if (newn == NULL)
-	{
 		return (NULL);
-	}
 	newn->n = value;
 	newn->left = NULL;
 	newn->right = NULL;
 	newn->parent = parent;
-	
 	return (newn);
 }

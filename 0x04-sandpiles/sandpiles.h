393dd0857440f274708acb2440483499fbb3534b
Juan Camilo Acevedo <60371540+acamilojuan@users.noreply.github.com>
2021-08-18 12:57:03 -0500

    Create sandpiles.h

---
+++ 0x04-sandpiles/sandpiles.h
@@ -0,0 +1,11 @@
+  
+#ifndef _SANDPILES_
+#define _SANDPILES_
+#define len 3
+
+#include <stdio.h>
+
+void sandpiles_sum(int grid1[3][3], int grid2[3][3]);
+int stabilizer(int grid1[3][3], int copy[3][3]);
+
+#endif

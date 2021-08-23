56fca6b243e1f713136dafe686ef786a26f783b7
Juan Camilo Acevedo <60371540+acamilojuan@users.noreply.github.com>
2021-08-23 15:23:27 -0500

    Fixing errors

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -1,24 +1,22 @@
 #!/usr/bin/python3
-""" Check if cratess can be opened """
+"""
+    Check locked boxes
+"""
 
 
 def canUnlockAll(boxes):
-    """ Checks if box can be opened """
-    unlocked = [False] * len(boxes)
-    unlocked[0] = True
-    keys_dic = []
-
-    for key in boxes[0]:
-        keys_dic.append(key)
-
-    for key in keys_dic:
-        if ((key < len(boxes)) and (unlocked[key] is False)):
-            unlocked[key] = True
-            for row in boxes[key]:
-                if row not in keys:
-                    keys_dic.append(row)
-
-    if False in unlocked:
-        return False
-    else:
+    """
+        Method that checks if every box can be opened
+    """
+    unlocked = [0]
+    if len(boxes) == 0:
         return True
+    for box in unlocked:
+        for key in boxes[box]:
+            if key not in unlocked:
+                if key < len(boxes):
+                    unlocked.append(key)
+
+    if len(unlocked) == len(boxes):
+        return True
+    return False

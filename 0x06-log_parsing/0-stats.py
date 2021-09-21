b1fe8da744e61c0a6a0b1bd1456f601430df0966
Juan Camilo Acevedo <60371540+acamilojuan@users.noreply.github.com>
2021-09-21 17:00:58 -0500

    Fixing parse

--- 0x06-log_parsing/0-stats.py
+++ 0x06-log_parsing/0-stats.py
@@ -1,51 +1,48 @@
 #!/usr/bin/python3
-""" Parsing the logs """
-
-import sys
-
-status_code = {
-    '200': 0,
-    '301': 0,
-    '400': 0,
-    '401': 0,
-    '403': 0,
-    '404': 0,
-    '405': 0,
-    '500': 0
-}
-
-counts = {
-    'size': 0,
-    'lines': 1
-}
-
-
-def printv():
-    """status code and size"""
-    print('File size: {}'.format(counts['size']))
-    for key in sorted(status_code.keys()):
-        if status_code[key] > 0:
-            print('{}: {}'.format(key, status_code[key]))
-
-
-def read(line):
-    """status and codes"""
-    counts['size'] += int(line[-1])
-    if line[-2] in status_code:
-        status_code[line[-2]] += 1
-
+""" Reading and computing """
 
 if __name__ == '__main__':
+
+    import sys
+    import collections
+
+    count = 0
+    size = 0
+    status = {'200': 0, '301': 0, '400': 0, '401': 0,
+              '403': 0, '404': 0, '405': 0, '500': 0}
+
     try:
         for line in sys.stdin:
+            count += 1
+            parse = line.split()
+
             try:
-                read(line.split(' '))
+                size += int(parse[-1])
+
+                for keys in status.keys():
+                    if keys == parse[-2]:
+                        status[keys] += 1
             except:
                 pass
-            if counts['lines'] % 10 == 0:
-                printv()
-            counts['lines'] += 1
+
+            if count == 10:
+                print('File size: {}'.format(size))
+                od = collections.OrderedDict(sorted(status.items()))
+                for keys, values in od.items():
+                    if values != 0:
+                        print('{}: {}'.format(keys, values))
+                count = 0
+
     except KeyboardInterrupt:
-        printv()
+        print('File size: {}'.format(size))
+        od = collections.OrderedDict(sorted(status.items()))
+        for keys, values in od.items():
+            if values != 0:
+                print('{}: {}'.format(keys, values))
         raise
-    printv()
+
+    print('File size: {}'.format(size))
+    od = collections.OrderedDict(sorted(status.items()))
+    for keys, values in od.items():
+        if values != 0:
+                print('{}: {}'.format(keys, values))

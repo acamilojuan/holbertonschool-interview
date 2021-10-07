7b8931192927e2b8ddcab982dc3f0a607c8875c2
Juan Camilo Acevedo <60371540+acamilojuan@users.noreply.github.com>
2021-10-07 10:51:57 -0500

    Including headers

--- 0x08-palindrome_integer/palindrome.h
+++ 0x08-palindrome_integer/palindrome.h
@@ -1,6 +1,10 @@
-#ifndef PALINDROME_H
-#define PALINDROME_H
+#ifndef LISTS_H
+#define LISTS_H
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 
 int is_palindrome(unsigned long n);
 
-#endif /* PALINDROME_H */
+#endif /* LISTS_H */

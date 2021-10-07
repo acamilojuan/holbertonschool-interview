eac4b983113521eb1d8339cb1e0692f78562ace7
Juan Camilo Acevedo <60371540+acamilojuan@users.noreply.github.com>
2021-10-07 10:48:08 -0500

    Changing method

--- 0x06-log_parsing/0-stats.py
+++ 0x06-log_parsing/0-stats.py
@@ -1,48 +1,55 @@
 #!/usr/bin/python3
-""" Reading and computing """
+""" Class LogParsing """
+import re
+import sys
 
-if __name__ == '__main__':
 
-    import sys
-    import collections
+def print_log_parsing(CODES, file_size):
+    """
+    function that print parsing logs
+    args:
+        codes: is a dictionary of status code
+        file_size: is the size of status
+    """
+    print("File size: {}".format(file_size))
+    for key, value in sorted(CODES.items()):
+        print("{}: {}".format(key, value))
 
-    count = 0
+
+def run():
+    """"
+    function that search the status code and size number
+    """
+    PATTERN = '([\\d]{3})\\s([\\d]{1,4})$'
+    CODES = {}
+    STOP = 10
+    step = 1
     size = 0
-    status = {'200': 0, '301': 0, '400': 0, '401': 0,
-              '403': 0, '404': 0, '405': 0, '500': 0}
 
-    try:
-        for line in sys.stdin:
-            count += 1
-            parse = line.split()
+    while True:
+
+        try:
+            line = input()
+
+            status, file_size = re.search(PATTERN, line).group().split()
+
+            size += int(file_size)
 
             try:
-                size += int(parse[-1])
+                if CODES[status]:
+                    CODES[status] += 1
+            except KeyError:
+                CODES[status] = 1
 
-                for keys in status.keys():
-                    if keys == parse[-2]:
-                        status[keys] += 1
-            except:
-                pass
+            if step == STOP:
+                print_log_parsing(CODES, size)
+                step = 1
 
-            if count == 10:
-                print('File size: {}'.format(size))
-                od = collections.OrderedDict(sorted(status.items()))
-                for keys, values in od.items():
-                    if values != 0:
-                        print('{}: {}'.format(keys, values))
-                count = 0
+            step += 1
+        except (KeyboardInterrupt, EOFError):
+            print_log_parsing(CODES, size)
+            exit()
 
-    except KeyboardInterrupt:
-        print('File size: {}'.format(size))
-        od = collections.OrderedDict(sorted(status.items()))
-        for keys, values in od.items():
-            if values != 0:
-                print('{}: {}'.format(keys, values))
-        raise
 
-    print('File size: {}'.format(size))
-    od = collections.OrderedDict(sorted(status.items()))
-    for keys, values in od.items():
-        if values != 0:
-                print('{}: {}'.format(keys, values))
+if __name__ == '__main__':
+    run()

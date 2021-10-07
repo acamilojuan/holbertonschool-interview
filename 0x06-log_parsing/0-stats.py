3efce9928f18e097c0d04d4b90c35abab93051f5
Juan Camilo Acevedo <60371540+acamilojuan@users.noreply.github.com>
2021-10-07 10:59:04 -0500

    Trying new algorithm

--- 0x06-log_parsing/0-stats.py
+++ 0x06-log_parsing/0-stats.py
@@ -1,43 +1,29 @@
 #!/usr/bin/python3
-'''
-    a script that reads stdin line by line and computes metrics
-'''
+"""script that reads stdin line by line and computes metrics"""
 
 
 import sys
 
 
-def addFileSize(data):
-    return int(data.split()[8])
-
-
-def codes(data):
-    return data.split()[7]
-
-i = 0
-s = 0
-stat = ['200', '301', '400', '401', '403', '404', '405', '500']
-new = [0, 0, 0, 0, 0, 0, 0, 0]
+file_size = 0
+status_codes = {"200": 0, "301": 0, "400": 0, "401": 0,
+                "403": 0, "404": 0, "405": 0, "500": 0}
 
 try:
-    for line in sys.stdin:
-        c = line.split()
-        if len(c) > 2:
-            if c[-2] in stat:
-                x = stat.index(c[-2])
-                new[x] = new[x] + 1
-            i += 1
-            s = s + int(c[-1])
-        if i == 10:
-            i = 0
-            print("File size: {}".format(s))
-            for x in range(8):
-                if new[x] != 0:
-                    print('{}: {}'.format(stat[x], new[x]))
-except Exception:
-    pass
+    for i, line in enumerate(sys.stdin, 1):
+        splited = line.split(" ")
+        if len(splited) < 2:
+            continue
+        if splited[-2] in status_codes:
+            status_codes[splited[-2]] += 1
+        file_size += eval(splited[-1])
+        if i % 10 == 0:
+            print("File size: {}".format(file_size))
+            for key, value in sorted(status_codes.items()):
+                if value > 0:
+                    print("{}: {}".format(key, value))
 finally:
-    print("File size: {}".format(s))
-    for x in range(8):
-        if new[x] != 0:
-            print("{}: {}".format(stat[x], new[x]))
+    print("File size: {}".format(file_size))
+    for key, value in sorted(status_codes.items()):
+        if value > 0:
+            print("{}: {}".format(key, value))

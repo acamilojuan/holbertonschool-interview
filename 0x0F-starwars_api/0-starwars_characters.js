2a673255dea3cac34f47d557a8fab491f67e2f6b
Juan Camilo Acevedo <60371540+acamilojuan@users.noreply.github.com>
2021-12-26 17:35:55 -0500

    Update 0-starwars_characters.js

--- 0x0F-starwars_api/0-starwars_characters.js
+++ 0x0F-starwars_api/0-starwars_characters.js
@@ -1,23 +1,16 @@
-
 #!/usr/bin/node
-const request = require('request');
-const process = require('process');
-const id = process.argv[2];
-const url = 'https://swapi-api.hbtn.io/api/films/' + id;
-request({ url: url }, (err1, res, body) => {
-  const nid = [];
-  const bjs = JSON.parse(body);
-  bjs.characters.forEach(urls => {
-    const id = urls.split('/')[5];
-    request({ url: urls }, (err2, res, body) => {
-      const bjs1 = JSON.parse(body);
-      nid[id] = bjs1.name;
+
+const r = require('request');
+const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
+
+r.get(url, (err, res, body) => {
+  if (err) console.log(err);
+  else {
+    JSON.parse(body).characters.forEach(character => {
+      r.get(character, (err, res, body) => {
+        if (err) console.log(err);
+        else console.log(JSON.parse(body).name);
+      });
     });
-  });
-  setTimeout(function () {
-    bjs.characters.forEach(element => {
-      const id = element.split('/')[5];
-      console.log(nid[id]);
-    });
-  }, 1000);
+  }
 });

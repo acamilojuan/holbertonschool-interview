7d14acfc48ed72b4adf884cb1f97bd8fdcec8c0d
Juan Camilo Acevedo <60371540+acamilojuan@users.noreply.github.com>
2021-12-26 17:40:57 -0500

    Update 0-starwars_characters.js

--- 0x0F-starwars_api/0-starwars_characters.js
+++ 0x0F-starwars_api/0-starwars_characters.js
@@ -1,41 +1,23 @@
 #!/usr/bin/node
-// prints all characters of a Star Wars movie
+
 const request = require('request');
+const url = `https://swapi-api.hbtn.io/api/films/${process.argv[2]}`;
 
-function catchMainData (indexPage) {
-  const firstUrl = `https://swapi-api.hbtn.io/api/films/${indexPage}/`;
-  return new Promise((resolve, reject) => {
-    request(firstUrl, function (error, res, body) {
-      if (!error && res.statusCode === 200) {
-        resolve(body);
-      } else {
-        reject(error);
-      }
+function getData (url) {
+    return new Promise((resolve, reject) => {
+        request(url, (err, response, body) => {
+            if (err) reject(err);
+            resolve(JSON.parse(body));
+        });
     });
-  });
 }
 
-function catchPeople (character) {
-  return new Promise((resolve, reject) => {
-    request(character, (error, res, body) => {
-      if (!error && res.statusCode === 200) {
-        resolve(body);
-      } else {
-        reject(error);
-      }
+async function getNames () {
+    const res = await getData(url);
+    res.characters.forEach(async function (character) {
+        const chrtr = await getData(character);
+        console.log(chrtr.name);
     });
-  });
 }
 
-async function main () {
-  const argv = +process.argv[2];
-  const data = await catchMainData(argv);
-  const charactersFilms = JSON.parse(data).characters;
-
-  for (const i in charactersFilms) {
-    const people = await catchPeople(charactersFilms[i]);
-    console.log(JSON.parse(people).name);
-  }
-}
-
-main();
+getNames();

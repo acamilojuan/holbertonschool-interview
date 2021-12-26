bcaa4e3fb135c576b5bfdc0060d9986f97ffb0b3
Juan Camilo Acevedo <60371540+acamilojuan@users.noreply.github.com>
2021-12-26 17:48:08 -0500

    Update 0-nqueens.py

--- 0x0C-nqueens/0-nqueens.py
+++ 0x0C-nqueens/0-nqueens.py
@@ -1,51 +1,42 @@
 #!/usr/bin/python3
-''' N queens algorithm '''
+"""backtrack n_queens"""
 import sys
 
-if len(sys.argv) != 2:
-    print("Usage: nqueens N")
-    exit(1)
 
-try:
-    n_q = int(sys.argv[1])
-except ValueError:
-    print("N must be a number")
-    exit(1)
-
-if n_q < 4:
-    print("N must be at least 4")
-    exit(1)
-
-
-def solve_nqueens(n):
-    '''self descriptive'''
-    if n == 0:
-        return [[]]
-    inner_solution = solve_nqueens(n - 1)
-    return [solution + [(n, i + 1)]
-            for i in range(n_q)
-            for solution in inner_solution
-            if safe_queen((n, i + 1), solution)]
-
-
-def attack_queen(square, queen):
-    '''self descriptive'''
-    (row1, col1) = square
-    (row2, col2) = queen
-    return (row1 == row2) or (col1 == col2) or\
-        abs(row1 - row2) == abs(col1 - col2)
-
-
-def safe_queen(sqr, queens):
-    '''self descriptive'''
-    for queen in queens:
-        if attack_queen(sqr, queen):
+def check_queen(p):
+    """checks for position availability"""
+    for i in range(len(p) - 1):
+        d = p[i] - p[len(p) - 1]
+        if d == 0 or d * (-1) == len(p) - 1 - i or d == len(p) - 1 - i:
             return False
     return True
 
 
-for answer in reversed(solve_nqueens(n_q)):
-    result = []
-    for p in [list(p) for p in answer]:
-        result.append([i - 1 for i in p])
-    print(result)
+def n_queen(N, r, sol, p):
+    """appends to and pops from solution"""
+    if r == N:
+        print(sol)
+    else:
+        for i in range(N):
+            p.append(i)
+            sol.append([r, i])
+            if check_queen(p):
+                n_queen(N, r + 1, sol, p)
+            p.pop()
+            sol.pop()
+
+
+if len(sys.argv) < 2:
+    print("Usage: nqueens N")
+    sys.exit(1)
+n = int(sys.argv[1])
+if type(n) != int:
+    print("N must be a number")
+    sys.exit(1)
+if n < 4:
+    print("N must be at least 4")
+    sys.exit(1)
+solution = []
+pos = []
+row = 0
+n_queen(n, row, solution, pos)

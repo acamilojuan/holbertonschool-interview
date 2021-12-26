fcd531b085e2fa0ca71190f2e955c8dd1e5ee8e7
Juan Camilo Acevedo <60371540+acamilojuan@users.noreply.github.com>
2021-12-26 17:29:48 -0500

    Update 0-starwars_characters.js

--- 0x0F-starwars_api/0-starwars_characters.js
+++ 0x0F-starwars_api/0-starwars_characters.js
@@ -1,24 +1,22 @@
-
 #!/usr/bin/node
 
 const request = require('request');
-const movieID = process.argv[2];
-const url = 'https://swapi-api.hbtn.io/api/films/' + movieID;
-const charDict = {};
+const filmId = process.argv[2];
+const url = `https://swapi-api.hbtn.io/api/films/${filmId}`;
 
-request(url, function (err, res, body) {
-  if (err) console.log(err);
-  const characters = JSON.parse(body).characters;
-  characters.forEach((char) => {
-    request(char, function (err, res, body) {
-      if (err) console.log(err);
-      const name = JSON.parse(body).name;
-      charDict[char] = name;
-      if (Object.values(charDict).length === characters.length) {
-        characters.forEach((character) => {
-          console.log(charDict[character]);
-        });
-      }
+request(url, async (err, response, body) => {
+  if (err) {
+    console.log(err);
+  }
+  for (const characterId of JSON.parse(body).characters) {
+    await new Promise((resolve, reject) => {
+      request(characterId, (err, response, body) => {
+        if (err) {
+          reject(err);
+        }
+        console.log(JSON.parse(body).name);
+        resolve();
+      });
     });
-  });
+  }
 });

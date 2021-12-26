3901ac6d6cefad57714fd2915c502b0f81e0b02d
Juan Camilo Acevedo <60371540+acamilojuan@users.noreply.github.com>
2021-12-26 17:28:23 -0500

    Update 0-starwars_characters.js

--- 0x0F-starwars_api/0-starwars_characters.js
+++ 0x0F-starwars_api/0-starwars_characters.js
@@ -1,37 +1,24 @@
+
 #!/usr/bin/node
 
-const requestOld = require('request');
-const util = require('util');
+const request = require('request');
+const movieID = process.argv[2];
+const url = 'https://swapi-api.hbtn.io/api/films/' + movieID;
+const charDict = {};
 
-const request = util.promisify(requestOld);
-
-const myArgs = process.argv.slice(2);
-
-if (myArgs.length !== 1) {
-  console.log("Invalid number of arguments, use like: './starwars 3'");
-  process.exit(1);
-}
-
-const movieNum = myArgs[0];
-
-async function main () {
-  const rawData = await request(`https://swapi-api.hbtn.io/api/films/${movieNum}/`);
-
-  const data = JSON.parse(rawData.body);
-
-  const characters = [];
-
-  for (let i = 0; i < data.characters.length; i++) {
-    characters.push(request(data.characters[i]).then((result) =>
-      JSON.parse(result.body)
-    ));
-  }
-
-  const charactersResult = await Promise.all(characters);
-
-  for (let i = 0; i < charactersResult.length; i++) {
-    console.log(charactersResult[i].name);
-  }
-}
-
-main();
+request(url, function (err, res, body) {
+  if (err) console.log(err);
+  const characters = JSON.parse(body).characters;
+  characters.forEach((char) => {
+    request(char, function (err, res, body) {
+      if (err) console.log(err);
+      const name = JSON.parse(body).name;
+      charDict[char] = name;
+      if (Object.values(charDict).length === characters.length) {
+        characters.forEach((character) => {
+          console.log(charDict[character]);
+        });
+      }
+    });
+  });
+});

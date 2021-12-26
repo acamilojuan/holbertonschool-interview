d22a62cdc097597abc9feea0f6ca660cb42bb882
Juan Camilo Acevedo <60371540+acamilojuan@users.noreply.github.com>
2021-12-26 17:44:55 -0500

    Update 0-starwars_characters.js

--- 0x0F-starwars_api/0-starwars_characters.js
+++ 0x0F-starwars_api/0-starwars_characters.js
@@ -1,16 +1,19 @@
 #!/usr/bin/node
-
 const request = require('request');
-const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
-request(url, { json: true }, async (err, res, body) => {
-  if (err) { return console.log(err); }
-  const charurls = body.characters;
-  for (const charindx in charurls) {
-    const data = await new Promise((resolve, reject) => {
-      request(charurls[charindx], { json: true }, function (er, res, body) {
-        if (er) { reject(err); } else { resolve(body.name); }
+const movieId = process.argv[2];
+const url = `https://swapi-api.hbtn.io/api/films/${movieId}`;
+request(url, async (error, response, body) => {
+  if (!error) {
+    const characters = JSON.parse(body).characters;
+    for (const charId in characters) {
+      await new Promise((resolve, reject) => {
+        request(characters[charId], (error, response, body) => {
+          if (!error) {
+            console.log(JSON.parse(body).name);
+            resolve();
+          }
+        });
       });
-    });
-    console.log(data);
+    }
   }
 });

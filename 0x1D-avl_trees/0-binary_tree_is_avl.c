0326d4fadc4f213d2c0f935b5f5957fe6d4ef9f1
Juan Camilo Acevedo <60371540+acamilojuan@users.noreply.github.com>
2022-03-13 16:15:38 -0500

    Update 0-binary_tree_is_avl.c

--- 0x1D-avl_trees/0-binary_tree_is_avl.c
+++ 0x1D-avl_trees/0-binary_tree_is_avl.c
@@ -1,86 +1,102 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_is_avl - check if a binary tree is a valid AVL tree
- * @tree: pointer to root node of tree
- * Return: 1 if AVL tree, else 0
+ * tree_height - measures the height of a binary tree
+ * @tree: pointer to the root node of the tree to measure the height
+ *
+ * Return: Height or 0 if tree is NULL
  */
-int binary_tree_is_avl(const binary_tree_t *tree)
+size_t tree_height(const binary_tree_t *tree)
 {
-	int lheight, rheight;
+	size_t height_l = 0;
+	size_t height_r = 0;
 
-	if (tree == NULL)
+	if (!tree)
 		return (0);
 
-	lheight = custom_binary_tree_height(tree->left);
-	rheight = custom_binary_tree_height(tree->right);
+	if (tree->left)
+		height_l = 1 + tree_height(tree->left);
 
-	if (abs(lheight - rheight) <= 1)
-		return (binary_tree_is_bst(tree));
+	if (tree->right)
+		height_r = 1 + tree_height(tree->right);
+
+	if (height_l > height_r)
+		return (height_l);
+	return (height_r);
+}
+
+/**
+ * isBST - check if each node has in the left a less value than himself
+ * @root: node to check
+ * @min: min value to check with the node at the left
+ * @max: max value to check with the node at the right
+ * Return: 1 if is a BST or 0 otherwise
+ */
+int isBST(const binary_tree_t *root,
+		  const binary_tree_t *min, const binary_tree_t *max)
+{
+	if (root == NULL)
+		return (1);
+
+	if (min != NULL && root->n <= min->n)
+		return (0);
+
+	if (max != NULL && root->n >= max->n)
+		return (0);
+
+	return (isBST(root->left, min, root) &&
+			isBST(root->right, root, max));
+}
+/**
+ * tree_is_bst - checks if a binary tree is a nid Binary Search Tree
+ * @tree: a pointer to the node node of the tree to check
+ * Return: 1 if tree is a nid BST, and 0 otherwise
+ * If tree is NULL, return 0
+ */
+int tree_is_bst(const binary_tree_t *tree)
+{
+	if (!tree)
+		return (0);
+
+	return (isBST(tree, NULL, NULL));
+}
+
+/**
+ * avl_check - checks if a binary tree is a valid AVL Tree
+ * @tree: pointer to the root node of the tree to check
+ *
+ * Return: 1 if tree is a valid AVL Tree, and 0 otherwise
+ */
+int avl_check(const binary_tree_t *tree)
+{
+	int diff, heightL = 0, heightR = 0;
+
+	if (!tree)
+		return (1);
+
+	if (!tree_is_bst(tree))
+		return (0);
+
+	heightL = tree_height(tree->left);
+	heightR = tree_height(tree->right);
+
+	diff = abs(heightL - heightR);
+
+	if (diff == 0 && avl_check(tree->left) && avl_check(tree->right))
+		return (1);
 	return (0);
 }
 
 /**
- * custom_binary_tree_height - Measure the height
- * of a binary tree from a given node
- * @tree: pointer to node of tree to measure
- * Description: Edited to work with balance factor function
- * Return: height of tree or 0 if NULL
- */
-int custom_binary_tree_height(const binary_tree_t *tree)
-{
-	int left, right;
-
-	if (tree == NULL)
-		return (0);
-
-	if (tree->left == NULL && tree->right == NULL)
-		return (1);
-
-	left = custom_binary_tree_height(tree->left) + 1;
-	right = custom_binary_tree_height(tree->right) + 1;
-
-	if (left > right)
-		return (left);
-	else
-		return (right);
-}
-
-/**
- * helper - help function
- * @tree: - tree
- * @low: low node
- * @hig: hig node
+ * binary_tree_is_avl - checks if a binary tree is a valid AVL Tree
+ * @tree: pointer to the root node of the tree to check
  *
- * Return: 1 or 0
+ * Return: 1 if tree is a valid AVL Tree, and 0 otherwise
  */
-int helper(const binary_tree_t *tree, int low, int hig)
+int binary_tree_is_avl(const binary_tree_t *tree)
 {
 	if (!tree)
 		return (0);
 
-	if (low > tree->n || hig < tree->n)
-		return (0);
-
-	return (helper(tree->left, low, tree->n - 1) &&
-			helper(tree->right, tree->n + 1, hig));
-
-	return (1);
-}
-
-/**
- * binary_tree_is_bst - function that checks if a binary tree
- * is a valid Binary Search Tree
- * @tree:  a pointer to the root node of the tree to check
- *
- * Return: value reurned by helper function
- */
-int binary_tree_is_bst(const binary_tree_t *tree)
-{
-	int num = 1;
-
-	if (!tree)
-		return (0);
-
-	return (helper(tree, num, num));
+	return (avl_check(tree));
 }

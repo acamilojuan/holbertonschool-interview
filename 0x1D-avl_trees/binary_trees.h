59a7a18e3f6500cc5541c392e15b51abd93643b5
Juan Camilo Acevedo <60371540+acamilojuan@users.noreply.github.com>
2022-03-13 16:16:28 -0500

    Update binary_trees.h

--- 0x1D-avl_trees/binary_trees.h
+++ 0x1D-avl_trees/binary_trees.h
@@ -1,10 +1,9 @@
-#ifndef _BINARY_TREES_H_
-#define _BINARY_TREES_H_
+#ifndef BINARY_TREES_H
+#define BINARY_TREES_H
 
 #include <stddef.h>
 #include <stdlib.h>
 #include <stdio.h>
-#include <string.h>
 
 /**
  * struct binary_tree_s - Binary tree node
@@ -14,18 +13,19 @@
  * @left: Pointer to the left child node
  * @right: Pointer to the right child node
  */
-typedef struct binary_tree_s
+struct binary_tree_s
 {
 	int n;
 	struct binary_tree_s *parent;
 	struct binary_tree_s *left;
 	struct binary_tree_s *right;
-} binary_tree_t;
+};
+
+typedef struct binary_tree_s binary_tree_t;
+typedef struct binary_tree_s avl_t;
 
 void binary_tree_print(const binary_tree_t *);
-int custom_binary_tree_height(const binary_tree_t *tree);
+
 int binary_tree_is_avl(const binary_tree_t *tree);
-int binary_tree_is_bst(const binary_tree_t *tree);
-int helper(const binary_tree_t *tree, int low, int hig);
 
 #endif /* _BINARY_TREES_H_ */
